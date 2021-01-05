8da85481d20065bd5294f5746f65307a66e589e8
Shynn Lawrence <61219106+shua1090@users.noreply.github.com>
2021-01-04 19:22:56 -0800

    Prepared for HashTable; Finished Basic Stack

--- Structures/dataStructs.hpp
+++ Structures/dataStructs.hpp
@@ -1,28 +1,37 @@
 /*
 Stack Class for LIFO
 */
-template <class c>
+template <class cls>
 class stack{
     private:
-        int length;
-        c* head;
-        int current;
+        int length; // Amount of elements in the stack; Currently immutable
+        cls* head; // Starting Memory Address of the stack
+        int current; // Current "node"
     public:
-        stack(c length);
-        ~stack();
+        stack(cls length); // Constructor
+        ~stack(); // Destructor
         int size(); // Return the predetermined size of stack
 
         // Official Stack Methods
-        int push(c element); // Push an element to the stack
-        c pop(); // Pop off the end of the stack
+        int push(cls element); // Push an element to the stack
+        cls pop(); // Pop off the end of the stack
 
         // Getting without Popping
-        c peek(int index) const;
-        c operator[] (int index) const;
+        cls peek(int index) const;
+        cls operator[] (int index) const;
 
         // Search Methods
-        int search(c obj) const; // Simple Search. ~ Linear Time.
+        int search(cls obj) const; // Simple Search. ~ Linear Time.
 
         // Overloads
         std::string toString() const;
+};
+
+template <class keycls, class valcls>
+class HashTable{
+    private:
+        int hashFunction(keycls c);
+    public:
+        HashTable();
+        ~HashTable();
 };
--- Structures/stack.cpp
+++ Structures/stack.cpp
@@ -1,60 +1,59 @@
-#include <cstdlib>
-#include <iostream>
 #include "dataStructs.hpp"
+#include <cstdlib>
 
-template <class c>
-stack<c>::stack(c length) {
-    this->head = (c*) calloc(length, sizeof(c));
+template <class cls>
+stack<cls>::stack(cls length) {
+    this->head = (cls*) calloc(length, sizeof(cls));
     this->length = length;
     current = 0;
 }
 
-template <class c>
-stack<c>::~stack() {
+template <class cls>
+stack<cls>::~stack() {
     free(this->head);
 }
 
-template <class c>
-int stack<c>::size() {
+template <class cls>
+int stack<cls>::size() {
     return this->length;
 }
 
-template <class c>
-int stack<c>::push(c element) {
+template <class cls>
+int stack<cls>::push(cls element) {
     if (this->current >= this->length){
         throw std::out_of_range("Pushing is impossible due to lack of allocated storage; Please reallocate the stack or pop.");
         return 1;
     } else {
-        *(this->head+this->current) = element;
+        this->head[current]= element;
         current += 1;
     }
     return 0;
 }
 
-template <class c>
-c stack<c>::pop () {
+template <class cls>
+cls stack<cls>::pop () {
     printf("Popping");
-    c result = head[--current];
-    head[this->current] = (c) NULL;
+    cls result = this->head[--current];
+    this->head[this->current] = (cls) NULL;
     return result;
 }
 
-template <class c>
-c stack<c>::peek(int index) const {
+template <class cls>
+cls stack<cls>::peek(int index) const {
     if (index > this->length || index < 0) {
         throw std::out_of_range("Unfortunately, your given Index was out of the valid range");
     } else {
-        return head[index];
+        return this->head[index];
     }
 }
 
-template <class c>
-c stack<c>::operator[] (int index) const {
+template <class cls>
+cls stack<cls>::operator[] (int index) const {
     peek(index);
 }
 
-template <class c>
-int stack<c>::search(c obj) const{
+template <class cls>
+int stack<cls>::search(cls obj) const{
     for (int i = 0; i < this->current; i++){
         if (head[i] == obj){
             return i;
@@ -63,19 +62,4 @@ int stack<c>::search(c obj) const{
         }
     }
     return -1; // Could not find the object with current method.
-}
-
-// template <class c>
-// std::string stack<c>::toString () const {
-//     return std::string("Stack object with head at ") + std::string(this->head) + std::string(" and size of ") + std::to_string(this->length);
-// }
-
-// Testing only
-int main(){
-    stack<int> f(10);
-    for (int i = 1; i < 11; i++){
-        f.push(i);
-    }
-
-    return 0;
 }
