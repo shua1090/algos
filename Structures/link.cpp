a05b87829952d64740a28d86361d9cb035a3f652
Aditya Matam <aditya0310@outlook.com>
2021-01-06 09:13:04 -0800

    Added methods to linked list

--- Structures/link.cpp
+++ Structures/link.cpp
@@ -5,7 +5,7 @@ struct Node {
     Node* next;
     Node() = default;
     Node(int value, Node* next=nullptr) : value(value), next(next) {}
-    ~Node() { free(next); }
+    ~Node() { delete next; }
 };
 
 struct LinkedList {
@@ -13,13 +13,19 @@ struct LinkedList {
     size_t size;
     LinkedList(Node* head) {
         this->head = head;
-        this->size = 0;
+        this->size = 1;
     }
-    ~LinkedList() { 
-        delete head; 
+    LinkedList(int first) {
+        this->head = new Node(first);
+        this->size = 1;
     }
+    ~LinkedList() { delete head; }
     void print() {
         Node* n = this->head;
+        if (n == nullptr) {
+            std::cout << "[]" << std::endl;
+            return;
+        }
         std::cout << "[";
         std::cout << n->value << " ";
         while (n->next != nullptr) {
@@ -28,27 +34,55 @@ struct LinkedList {
         }
         std::cout << "]" << std::endl;
     }
-    Node* getTail() {
+    Node* iterate(int i=-1) const {
+        if (this->head == nullptr)
+            return this->head;
+
         Node* n = this->head;
-        while (n->next != nullptr)
-            n = n->next;
+        if (i == -1) {
+            while (n->next != nullptr)
+                n = n->next;
+        }
+        else {
+            for (int k = 0; k < i; k++)
+                n = n->next;
+        }
         return n;
     }
     void append(int value) {
-        this->size++;
         Node* next = new Node(value);
-        Node* tail = getTail();
+        Node* tail = iterate();
         tail->next = next;
+        this->size++;
     }
-    int operator[](int i) const {
-        if (i >= this->size || i < 0) {
-            std::cout << "INVALID INDEX" << std::endl;
-            return -1;
+    void extend(const LinkedList& other) {
+        Node* tail = iterate();
+        tail = other.head;
+        this->size += other.size;
+    }
+    void insert(int index, int value) {
+        if (index < 0 || index > this->size)
+            throw std::exception();
+        if (index == 0) {
+            Node* next = new Node(value, this->head);
+            this->head = next;
         }
-        Node* n = this->head;
-        for (int k = 0; k < i; k++)
-            n = n->next;
-        return n->value;
+        else if (index == this->size) {
+            append(value);
+        }
+        else {
+            Node* left = iterate(index - 1);
+            Node* right = left->next;
+            Node* next = new Node(value, right);
+            left->next = next;
+        }
+        this->size++;
+    }
+    int& operator[](int i) const {
+        if (i < 0 || i >= this->size)
+            throw std::exception();
+        Node* out = iterate(i);
+        return out->value;
     }
 };
 
@@ -56,9 +90,13 @@ int main() {
     Node* head = new Node(10);
     LinkedList nums(head);
     nums.print();
+
     nums.append(5);
-    nums.append(12);
+    nums.append(500);
     nums.append(25);
-    std::cout << nums[1] << std::endl;
+    nums.insert(4, 12);
+
     nums.print();
+
+    std::cout << nums[1] << std::endl;
 }
